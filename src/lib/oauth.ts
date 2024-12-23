bf614db0bac6ada6e344977b9379397ef3083b6b
pankaj11koundal <pankaj11koundal@gmail.com>
2024-12-23 12:12:07 +0530

    sign-in url modefied-3

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -2,7 +2,6 @@
 
 import { createAdminClient } from "@/lib/appwrite";
 import { redirect } from "next/navigation";
-import { headers } from "next/headers";
 import { OAuthProvider } from "node-appwrite";
 
 export async function signUpWithGithub() {
