6b9ba9806d608200223b1e7ccf91a34350241fd6
pankaj11koundal <pankaj11koundal@gmail.com>
2024-12-24 10:37:47 +0530

    pushing cookies

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -9,7 +9,7 @@ export async function createSessionClient() {
     .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
-  const session = await cookies().get(AUTH_COOKIE);
+  const session = cookies().get(AUTH_COOKIE);
 
   if (!session || !session.value) throw new Error("Unauthorized");
 
